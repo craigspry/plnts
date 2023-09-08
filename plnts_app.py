f746f7e158c995126bcb9da347a3ca0327be4b6d
Craig Spry <craig.spry@gmail.com>
2023-09-08 21:08:03 +1000

    added requirements and formatted, lets call it that, html output for browsers

--- plnts_app.py
+++ plnts_app.py
@@ -1,6 +1,6 @@
 import cherrypy
 import ephem
-from datetime import datetime 
+
 
 def get_obj_details(obj, name):
     # Melbourne, Australia coordinates
@@ -23,38 +23,42 @@ def get_obj_details(obj, name):
     else:
         rising_time = observer.previous_rising(obj)
     setting_time = observer.next_setting(obj)
-    return (elevation, azimuth, rising_time, setting_time)
+    transit_time = observer.next_transit(obj)
+    return (elevation, azimuth, rising_time, setting_time, transit_time)
+
 
 def pretty_print_obj(obj, name):
     details = get_obj_details(obj, name)
     pretty_name = name.ljust(15)
-    return f"  {pretty_name}| {details[0]:>7.2f} | {details[1]:>7.2f} | {details[2].datetime():%Y-%m-%d %H:%M:%S} | {details[3].datetime():%Y-%m-%d %H:%M:%S}\n"
+    return f"  {pretty_name}| {details[0]:>7.2f} | {details[1]:>7.2f} | {details[2].datetime():%Y-%m-%d %H:%M:%S} | {details[3].datetime():%Y-%m-%d %H:%M:%S} | {details[4].datetime():%Y-%m-%d %H:%M:%S}\n"
 
-def get_rise_set(obsv, obj):
-    # obj.compute(obsv)
-    if obj.alt < 0:
-        return obsv.next_rising(obj), obsv.next_transit(obj), obsv.next_setting(obj), obj.az, obj.alt
-    else:
-        return obsv.previous_rising(obj).datetime(), obsv.next_transit(obj), obsv.next_setting(obj), obj.az, obj.alt
 
 class Planets(object):
     @cherrypy.expose
     def index(self):
-        rtn = "  Body           |Elevation|Azimuth  |Rise Time            |Set Time\n"
-        rtn += "----------------------------------------------------------------------------------\n"
+        user_agent = cherrypy.request.headers.get('User-Agent')
+        rtn = "  Body           |Elevation|Azimuth  |Rise Time            |Set Time             |Transit Time\n"
+        rtn += "--------------------------------------------------------------------------------------------------------\n"
         sun = ephem.Sun()
         rtn += pretty_print_obj(sun, "Sun")
+        mercury = ephem.Mercury()
+        rtn += pretty_print_obj(mercury, "Mercury")
+        venus = ephem.Venus()
+        rtn += pretty_print_obj(venus, "Venus")
         moon = ephem.Moon()
         rtn += pretty_print_obj(moon, "Moon")
-        saturn = ephem.Saturn()
-        rtn += pretty_print_obj(saturn, "Saturn")
-        jupiter = ephem.Jupiter()
-        rtn += pretty_print_obj(jupiter, "Jupiter")
         mars = ephem.Mars()
         rtn += pretty_print_obj(mars, "Mars")
-        cherrypy.response.headers['Content-Type'] = 'plain/text'
+        jupiter = ephem.Jupiter()
+        rtn += pretty_print_obj(jupiter, "Jupiter")
+        saturn = ephem.Saturn()
+        rtn += pretty_print_obj(saturn, "Saturn")
+        if 'curl' not in user_agent:
+            rtn = "<html><body>" + rtn.replace('\n', '<br>') + "</body></html>"
+        else:
+            cherrypy.response.headers['Content-Type'] = 'plain/text'
         return str.encode(rtn)
 
-if __name__ == '__main__':
-   cherrypy.quickstart(Planets(), '/')
 
+if __name__ == '__main__':
+    cherrypy.quickstart(Planets(), '/')
